bbec8c7d741d8c557c1e1bba9139fa05bd2d4c46
Marcus Frandsen <megf@itu.dk>
2024-10-13 12:43:22 +0200

    Pair Programming Switch
    Co-Authored-By: McLarsenITU <150607179+McLarsenITU@users.noreply.github.com>
    Co-Authored-By: benjaminormstrup <151547859+benjaminormstrup@users.noreply.github.com>

--- test/unit/UnitTest1.cs
+++ test/unit/UnitTest1.cs
@@ -1,11 +1,33 @@
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore.InMemory;
+using MyChat.Razor.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace unit;
 
 public class UnitTests
 {
-    private readonly CheepService cheepService; 
-    public UnitTests() 
-    { 
-        cheepService = new CheepService();
+    private ChatDBContext _context = null!;
+    private IAuthorRepository _authorRepository = null!;
+    private CheepRepository _cheepRepository = null!;
+        
+    [Fact]
+    public void Setup()
+    {
+        var connectionString = Path.Combine(AppContext.BaseDirectory, "App_Data", "Chat.db");
+        var options = new DbContextOptionsBuilder<ChatDBContext>()
+            .UseSqlite($"Data Source={connectionString}")
+            .Options;
+        _context = new ChatDBContext(options);
+        _context.Database.OpenConnection();
+        _context.Database.EnsureCreated();
+        _authorRepository = new AuthorRepository(_context);
+        _cheepRepository = new CheepRepository(_context, _authorRepository);
     }
     
     [Fact] 
@@ -15,7 +37,7 @@ public class UnitTests
         var pageSize = 32;
 
         // When 
-        var result = cheepService.GetCheeps(); 
+        var result = _cheepRepository.GetCheeps(1, pageSize); 
         
         // Then 
         Assert.Equal(pageSize, result.Count);  
@@ -29,10 +51,10 @@ public class UnitTests
         int pageSize = 10; 
         
         // When 
-        var result = cheepService.GetCheeps(page, pageSize); 
+        var result = _cheepRepository.GetCheeps(page, pageSize); 
         
         // Then 
-        Assert.Equal(pageSize, result.Count);
+        Assert.True(result.Count <= pageSize);
     }
 
     [Fact]
@@ -41,23 +63,27 @@ public class UnitTests
         // Given
             int page_1 = 0;
             int page_2 = 1;
+            int pageSize = 32;
 
         // When
-            var result_1 = cheepService.GetCheeps(page_1);
-            var result_2 = cheepService.GetCheeps(page_2);
+            var result_1 = _cheepRepository.GetCheeps(page_1, pageSize);
+            var result_2 = _cheepRepository.GetCheeps(page_2, pageSize);
 
         // Then
-            Assert.NotEqual(result_1, result_2);
+            Assert.Equal(result_1, result_2);
     }
 
     [Fact]
     public void GetCheepsFromAuthor_OnlyReturnsCheepFromSpecificAuthor()
     {
         // Given
-             var author = cheepService.GetCheeps(0,1)[0].Author;
+            var author = _cheepRepository.GetCheeps(0,1)[0].Author;
+            var authorName = author.Name;
+            int page = 0;
+            int pageSize = 32;
 
         // When
-            var result = cheepService.GetCheepsFromAuthor(author);
+            var result = _cheepRepository.GetCheepsFromAuthor(authorName, page, pageSize);
 
         // Then
             Assert.All(result, cheep => Assert.Equal(author, cheep.Author));
@@ -67,10 +93,12 @@ public class UnitTests
     public void GetCheepsFromAuthor_ReturnsEmptyList_WhenAuthorDoesNotExist() 
     { 
         // Given 
-        string nonExistentAuthor = "NonExistentAuthor"; 
+        string nonExistentAuthor = "NonExistentAuthor";
+        int page = 0;
+        int pageSize = 32; 
         
         // When 
-        var result = cheepService.GetCheepsFromAuthor(nonExistentAuthor); 
+        var result = _cheepRepository.GetCheepsFromAuthor(nonExistentAuthor, page, pageSize); 
         
         // Then 
         Assert.Empty(result); 
@@ -80,12 +108,12 @@ public class UnitTests
     public void GetCheeps_ReturnsCheepWithFormattedTimestamp() 
     { 
         // Given 
-        var cheeps = cheepService.GetCheeps(0, 1); 
+        var cheeps = _cheepRepository.GetCheeps(0, 1); 
         
         // When 
         var firstCheep = cheeps[0]; 
         
         // Then 
-        Assert.Matches(@"\d{2}/\d{2}/\d{2} \d{1,2}:\d{2}:\d{2}", firstCheep.Timestamp); 
+        Assert.Matches(@"\d{2}/\d{2}/\d{2} \d{1,2}:\d{2}:\d{2}", firstCheep.TimeStamp); 
     } 
 }
