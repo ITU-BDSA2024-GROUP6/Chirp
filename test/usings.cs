0d6b84833bf77e00251c62af747feed3b1d66a4c
Benjamin Ormstrup <beor@itu.dk>
2024-10-30 12:34:49 +0100

    Update to database and test
    Deleted the old database.
    Created a new database and updated it using "dotnet ef database update"
    
    Deleted the test folder, since it had references to the old Razor project.
    
    These changes were made in order to test if the web app now shows content properly.
    
    Co-Authored-By: KongCristi <151643927+KongCristi@users.noreply.github.com>
    Co-Authored-By: Marcus Egede Frandsen <megf@itu.dk>
    Co-Authored-By: McLarsenITU <150607179+McLarsenITU@users.noreply.github.com>

--- test/usings.cs
+++
@@ -1,6 +0,0 @@
-global using System.Collections.Generic; 
-global using Xunit;
-global using System.Net.Http;
-global using System.Threading.Tasks;
-global using System;
-global using System.Text.Json;
