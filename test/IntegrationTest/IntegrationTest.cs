884c31c2c641db4c0e23c1e512390b7a28cb7e7d
Christian Jørgensen <christian.joergensen2@gmail.com>
2024-12-18 19:22:47 +0100

    IntegrationTest
    A new integrationTestClass was created.
    Note: needs cleanup with the previous Integration Test Class

---
+++ test/IntegrationTest/IntegrationTest.cs
@@ -0,0 +1,89 @@
+using Chirp.Core.RepositoryInterfaces;
+using Chirp.Infrastructure.Repositories;
+using Chirp.Infrastructure.Data;
+using Chirp.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IntegrationTests
+{
+    [Category("Integration")]
+    public class IntegrationTests : IDisposable
+    {
+        private ChatDBContext _context;
+        private ICheepRepository _cheepRepository;
+        private IAuthorRepository _authorRepository;
+
+        public IntegrationTests()
+        {
+            // Setup in-memory SQLite database
+            var options = new DbContextOptionsBuilder<ChatDBContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            _context = new ChatDBContext(options);
+
+            // Initialize repositories
+            _authorRepository = new AuthorRepository(_context);
+            _cheepRepository = new CheepRepository(_context, _authorRepository);
+        }
+
+        public void Dispose()
+        {
+            _context.Database.EnsureDeleted();
+            _context.Dispose();
+        }
+
+        [Test]
+        public async Task CreateCheepsAndGetByFollowers_ReturnsCorrectCheeps()
+        {
+            // Arrange
+            var author1 = new Author
+            {
+                Id = Guid.NewGuid().ToString(),
+                UserName = "Kong Christian",
+                Email = "ChristianTheKing@email.dk"
+            };
+
+            var author2 = new Author
+            {
+                Id = Guid.NewGuid().ToString(),
+                UserName = "Benjamin The Peasant",
+                Email = "BennyHarIkNogenVenner@email.dk"
+            };
+
+            _context.Authors.AddRange(author1, author2);
+            await _context.SaveChangesAsync();
+
+            // Retrieve authors from DB
+            var author1FromDb = _authorRepository.GetAuthorByName(author1.UserName);
+            var author2FromDb = _authorRepository.GetAuthorByName(author2.UserName);
+
+            // Null checks
+            Assert.IsNotNull(author1FromDb, "Author1 not found in database");
+            Assert.IsNotNull(author2FromDb, "Author2 not found in database");
+
+            // Follow Author
+            await _authorRepository.FollowAuthor(
+                author1FromDb?.UserName ?? throw new ArgumentNullException(nameof(author1FromDb)),
+                author2FromDb?.UserName ?? throw new ArgumentNullException(nameof(author2FromDb))
+            );
+
+            // Create Cheeps
+            await _cheepRepository.CreateCheep("Hello World", author1FromDb, DateTime.Now);
+            await _cheepRepository.CreateCheep("Another Cheep", author2FromDb, DateTime.Now);
+
+            // Act
+            var cheepsFromFollowers = _cheepRepository.GetUsersFollowingCheeps(author1FromDb, 0, 10);
+
+            // Assert
+            Assert.IsNotNull(cheepsFromFollowers, "Cheeps from followers should not be null");
+            Assert.That(cheepsFromFollowers.Count(), Is.EqualTo(1), "Should return 1 cheep");
+            Assert.That(cheepsFromFollowers.First().Text, Is.EqualTo("Another Cheep"), "The text of the cheep is incorrect");
+        }
+    }
+}
--- test/integration/IntegrationTest.cs
+++ test/integration/IntegrationTest.cs
@@ -1,15 +1,14 @@
 using Chirp.Core.RepositoryInterfaces;
 using Chirp.Infrastructure.Repositories;
-using Microsoft.EntityFrameworkCore;
-using NUnit.Framework;
-using Chirp.Infrastructure.Repositories;
 using Chirp.Infrastructure.Data;
 using Chirp.Core.Models;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
- 
+
 namespace IntegrationTests
 {
     [Category("Integration")]
@@ -18,83 +17,69 @@ namespace IntegrationTests
         private ChatDBContext _context;
         private ICheepRepository _cheepRepository;
         private IAuthorRepository _authorRepository;
- 
+
         public IntegrationTests()
         {
             // Setup in-memory SQLite database
             var options = new DbContextOptionsBuilder<ChatDBContext>()
                 .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
                 .Options;
- 
+
             _context = new ChatDBContext(options);
- 
+
             // Initialize repositories
-            _cheepRepository = new CheepRepository(_context);
             _authorRepository = new AuthorRepository(_context);
+            _cheepRepository = new CheepRepository(_context, _authorRepository);
         }
- 
+
         public void Dispose()
         {
             _context.Database.EnsureDeleted();
             _context.Dispose();
         }
- 
+
         [Test]
         public async Task CreateCheepsAndGetByFollowers_ReturnsCorrectCheeps()
         {
             // Arrange
             var author1 = new Author
             {
+                Id = Guid.NewGuid().ToString(),
                 UserName = "John Doe",
-                Email = "john@email.dk",
-                Cheeps = new List<Cheep>()
+                Email = "john@email.dk"
             };
- 
+
             var author2 = new Author
             {
+                Id = Guid.NewGuid().ToString(),
                 UserName = "Joe Smith",
-                Email = "joe@email.dk",
-                Cheeps = new List<Cheep>()
+                Email = "joe@email.dk"
             };
- 
-            _context.Authors.Add(author1);
-            _context.Authors.Add(author2);
+
+            _context.Authors.AddRange(author1, author2);
             await _context.SaveChangesAsync();
- 
+
             // Follow Author
             var author1FromDb = _authorRepository.GetAuthorByName(author1.UserName);
             var author2FromDb = _authorRepository.GetAuthorByName(author2.UserName);
- 
+
+            // Null checks
+            Assert.IsNotNull(author1FromDb, "Author1 not found in database");
+            Assert.IsNotNull(author2FromDb, "Author2 not found in database");
+
             await _authorRepository.FollowAuthor(author1FromDb.UserName, author2FromDb.UserName);
- 
+
             // Create Cheeps
-            var cheep1 = new Cheep
-            {
-                Id = "1",
-                Text = "Hello World",
-                TimeStamp = DateTime.Now,
-                Author = author1FromDb
-            };
- 
-            var cheep2 = new Cheep
-            {
-                Id = "2",
-                Text = "Another Cheep",
-                TimeStamp = DateTime.Now,
-                Author = author2FromDb
-            };
- 
-            await _cheepRepository.CreateCheep(cheep1);
-            await _cheepRepository.CreateCheep(cheep2);
- 
+            await _cheepRepository.CreateCheep("Hello World", author1FromDb, DateTime.Now);
+            await _cheepRepository.CreateCheep("Another Cheep", author2FromDb, DateTime.Now);
+
             // Act
-            var followers = new List<string> { author1FromDb.Id };
-            var cheepsFromFollowers = await _cheepRepository.GetByFollowers(followers, 0);
- 
+            var cheepsFromFollowers = _cheepRepository.GetUsersFollowingCheeps(author1FromDb, 0, 10);
+
             // Assert
-            Assert.IsNotNull(cheepsFromFollowers);
-            Assert.That(cheepsFromFollowers.Count(), Is.EqualTo(1));
-            Assert.That(cheepsFromFollowers.First().Text, Is.EqualTo("Another Cheep"));
+            Assert.IsNotNull(cheepsFromFollowers, "Cheeps from followers should not be null");
+            Assert.That(cheepsFromFollowers.Count(), Is.EqualTo(1), "Should return 1 cheep");
+            Assert.That(cheepsFromFollowers.First().Text, Is.EqualTo("Another Cheep"), "The text of the cheep is incorrect");
         }
     }
 }
